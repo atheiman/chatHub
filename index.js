752de37c16a95ef57caa3ed284222de48a678e25
Austin Heiman <atheiman@ip-172-31-34-254.us-west-2.compute.internal>
2014-07-15 04:01:46 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+  res.sendfile('/tmp/index.html');
+});
+
+io.on('connection', function(socket){
+	console.log('a user connected');
+	io.emit('chat message', 'a user connected');
+	
+	socket.on('chat message', function(msg){
+		console.log('message: ' + msg);
+		io.emit('chat message', msg);
+	});
+	
+	socket.on('disconnect', function(){
+		console.log('user disconnected');
+		io.emit('chat message', 'a user disconnected');
+	});
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
