be716d89a9a61ebd9405b23297d55fd1bf37e182
Chris Delpire <cadelpire@bellsouth.net>
2014-07-17 21:15:02 -0500

    added assets
    added assets folder which will hold things like icons etc.

---
+++ assets/functions.js
@@ -0,0 +1,15 @@
+function generateNoun() {
+    var nounArray = [ "Cat", "Dog", "Monkey", "Cow", "Moose", "Chicken",
+        "Platypus", "Mango", "Pirate", "Wizard", "Knight", "Box" ];
+    var noun = nounArray[Math.floor(Math.random() * nounArray.length)];
+    
+    return noun;
+}
+
+function generateAdj() {
+    var adjectiveArray = [ "Blue", "Red", "Metal", "Angry", "Feisty", "Sad",
+        "Hungry", "Mellow", "Happy", "Erect", "Aroused", "Deliberate", "Frenzied" ];
+    var adj = adjectiveArray[Math.floor(Math.random() * adjectiveArray.length)];
+
+return adj;
+}
